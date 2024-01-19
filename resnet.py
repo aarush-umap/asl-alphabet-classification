8baa3b6f1996b0939e793f4fe60cb591aaf21d31
Aarush Umap <aarush.umap@gmail.com>
2024-01-18 22:22:03 -0800

    print debugging

--- resnet.py
+++ resnet.py
@@ -44,8 +44,8 @@ class ResNet(Model):
                 if j != 0 or i == 0:
                     curr_stride = 1
                 fs = filter_sizes[i]
-                if j != 0:
-                    fs = filter_sizes[i] * 4
+                # if j != 0:
+                #     fs = filter_sizes[i] * 4
                 self.all_layers[f'conv_{i+1}_{j+1}'] = Block(filter_size=fs, stride=curr_stride) # TODO: fix filter size
         
         self.all_layers['global_avg_pool'] = GlobalAveragePooling2D()
@@ -64,25 +64,49 @@ class ResNet(Model):
 class Block(Layer):
     def __init__(self, filter_size, stride):
         super(Block, self).__init__()
+        self.fs = filter_size
         self.layers = {}
         self.layers['conv_1'] = Conv2DWithBN(filters=filter_size, kernel_size=1, strides=1, padding='valid')
         self.layers['conv_2'] = Conv2DWithBN(filters=filter_size, kernel_size=3, strides=stride, padding='same')
         self.layers['conv_3'] = Conv2DWithBN(filters=(filter_size * 4), kernel_size=1, strides=1, padding='valid')
-        self.layers['relu'] = ReLU()
-        self.layers['transform'] = Conv2DWithBN(filters=(filter_size * 4), kernel_size=1, strides=stride, padding='same')
+       
+        print(f'conv_1: Conv2DWithBN(filters={filter_size}, kernel_size={1}, strides={1}, padding=valid)')
+        print(f'conv_2: Conv2DWithBN(filters={filter_size}, kernel_size={3}, strides={stride}, padding=same)')
+        print(f'conv_3:Conv2DWithBN(filters=({filter_size} * 4), kernel_size={1}, strides={1}, padding=valid)')
+        print('relu')
         if stride == 1: self.dotted = False
         else: self.dotted = True
         
 
     def call(self, input, training):
+        if input.shape[2] != self.fs:
+            self.dotted = True
         x = input
-        for i in range(1,3):
-            x = self.layers[f'conv_{i}'](x, training)
+        for i in range(3):
+            x = self.layers[f'conv_{i+1}'](x, training)
         if self.dotted:
-            x = tf.add(x, self.layers['transform'](input, training))
+            y = self.layers['transform'](input, training)
+            # print(f'is dotted with x: {x.shape} and y: {y.shape}')
+            x = tf.add(x, y)
         else:
+            # print(f'is not dotted with x: {x.shape} and y: {input.shape}')
             x = tf.add(x, input)
         return self.layers['relu'](x)
     
 def ResNet50(img_shape, num_classes):
     return ResNet(img_shape=img_shape, num_classes=num_classes, repeats=[3,4,6,4])
+
+# a = tf.zeros([1,56,56,64])
+# b = tf.zeros([1,56,56,256])
+# conv_1 = Conv2DWithBN(filters=64, kernel_size=1, strides=1, padding='valid')
+# conv_2 = Conv2DWithBN(filters=64, kernel_size=3, strides=1, padding='same')
+# conv_3 = Conv2DWithBN(filters=(64 * 4), kernel_size=1, strides=1, padding='valid')
+# transform = Conv2DWithBN(filters=(64 * 4), kernel_size=1, strides=1, padding='same')
+# x = conv_1(a)
+# x = conv_2(x)
+# x = conv_3(x)
+# print("a shape: ", a.shape)
+# print("b shape: ", b.shape)
+# y = transform(b)
+# z = tf.add(x, y)
+# print(f"x: {x.shape} y: {y.shape} z: {z.shape}")
