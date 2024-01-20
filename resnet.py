6f1ba6661fcda583335972ee66a06adf5c0f0a5a
Aarush Umap <aarush.umap@gmail.com>
2024-01-19 18:01:00 -0800

    Completed documentation for classes

--- resnet.py
+++ resnet.py
@@ -37,32 +37,65 @@ class Conv2DWithBN(Layer):
         
 
 class ResNet(Model):
-    def __init__(self, img_shape, num_classes, repeats=[3,4,6,3]):
+    def __init__(self, num_classes, repeats=[3,4,6,3]):
+        """
+        A modular ResNet model based on different combinations of custom block layers. Models follow the same basic 
+        structure, starting with a 7x7 conv layer and 3x3 max pool, followed by skip connection blocks repeated
+        various amounts of times depending on the depth of the model, wrapped up by global average pooling and a
+        final dense layer mapping all filters to output classes (with softmax activation).
+
+        Input:
+        - num_classes: number of classification classes.
+        - repeats: list of ints that dictates how many blocks of each filter size [64, 128, 256, 512] will be 
+        added to the model.
+        """
         super(ResNet, self).__init__(name='ResNet')
         self.repeats = repeats
 
         self.all_layers = {}
+        # setup of the starting universal starting layers
         self.all_layers['conv_1'] = Conv2DWithBN(filters=64, kernel_size=7, strides=2, padding='same')
         self.all_layers['max_pool'] = MaxPooling2D(pool_size=3, strides=2, padding='same')
 
+        # specific setup of blocks using repeats to decide how many times each filter size is repeated
         filter_sizes = [64, 128, 256, 512]
         for i, repeat in enumerate(repeats):
             for j in range(repeat):
+                # dictates whether or now H and W will be halved
                 curr_stride = 2
                 if j != 0 or i == 0:
                     curr_stride = 1
                     
-                self.all_layers[f'conv_{i+2}_{j+1}'] = Block(filter_size=filter_sizes[i], stride=curr_stride, name=(f'conv_{i+2}_{j+1}')) # TODO: fix filter size
+                self.all_layers[f'conv_{i+2}_{j+1}'] = Block(filter_size=filter_sizes[i], stride=curr_stride, name=(f'conv_{i+2}_{j+1}'))
         
+        # final layers to map to output classes
         self.all_layers['global_avg_pool'] = GlobalAveragePooling2D()
         self.all_layers['fully_connected'] = Dense(units=num_classes, activation='softmax')
 
     def call(self, input, training):
+        """
+        The forward function that updates input after all conv filters, 
+        pooling, and fully connected layers. 
+
+        Input:
+        - input: tensor with shape (B x H x W x C).
+        - training: boolean that dictates whether batch normalization should update or use running parameters.
+
+        Returns:
+        - x: a tensor of shape (B x C) where C = number of classes and each ith value is the probability that 
+        the input was of class i
+        """
+
+        # First universal layers
         x = self.all_layers['conv_1'](input, training)
         x = self.all_layers['max_pool'](x)
+
+        # All blocks
         for i, repeat in enumerate(self.repeats):
             for j in range(repeat):
                 x = self.all_layers[f'conv_{i+2}_{j+1}'](x, training)
+
+        # Final mapping to classes
         x = self.all_layers['global_avg_pool'](x)
         x = self.all_layers['fully_connected'](x)
         return x
@@ -101,8 +134,7 @@ class Block(Layer):
         # if H and W dimensionality will be reduced by stride, accounts by transforming 
         # input to match the output shape
         if stride == 1: self.dotted = False
-        else: self.dotted = True 
-        
+        else: self.dotted = True     
 
     def call(self, input, training):
         """
@@ -136,11 +168,11 @@ class Block(Layer):
         
         return self.layers['relu'](x)
     
-def ResNet50(img_shape, num_classes):
-    return ResNet(img_shape=img_shape, num_classes=num_classes, repeats=[3,4,6,3])
+def ResNet50(num_classes):
+    return ResNet(num_classes=num_classes, repeats=[3,4,6,3])
 
-def ResNet101(img_shape, num_classes):
-    return ResNet(img_shape=img_shape, num_classes=num_classes, repeats=[3,4,23,3])
+def ResNet101(num_classes):
+    return ResNet(num_classes=num_classes, repeats=[3,4,23,3])
 
-def ResNet152(img_shape, num_classes):
-    return ResNet(img_shape=img_shape, num_classes=num_classes, repeats=[3,8,36,3])
+def ResNet152(num_classes):
+    return ResNet(num_classes=num_classes, repeats=[3,8,36,3])
